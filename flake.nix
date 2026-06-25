ac220d0ffccb8c23fc7fedd8b4690f5c93fa8a5d
matthis-k <matthis.kaelble@gmail.com>
2026-06-25 13:45:50 +0200

    Simplify flake, focus on minimal outline

--- flake.nix
+++ flake.nix
@@ -3,17 +3,8 @@
 
   inputs = {
     phenix-pins.url = "github:matthis-k/phenix-pins";
-    phenix-packages.url = "github:matthis-k/phenix-packages";
     nixpkgs.follows = "phenix-pins/nixpkgs";
   };
 
-  outputs = inputs: {
-    devShells.x86_64-linux.default = (import inputs.nixpkgs {
-      system = "x86_64-linux";
-    }).mkShell { };
-
-    devShells.aarch64-linux.default = (import inputs.nixpkgs {
-      system = "aarch64-linux";
-    }).mkShell { };
-  };
+  outputs = inputs: { };
 }
