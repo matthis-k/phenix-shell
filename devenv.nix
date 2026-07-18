7b1deab3f66701f10facdb3845904bf70c3f10c1
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-18 11:23:01 +0200

    refactor: import root maintenance module explicitly

--- devenv.nix
+++ devenv.nix
@@ -1,10 +1,4 @@
-{ lib, ... }:
-let
-  maintenanceModules = builtins.filter (path: builtins.baseNameOf path == "maintenance.nix") (
-    lib.filesystem.listFilesRecursive ./.
-  );
-in
-{
-  imports = maintenanceModules;
+_: {
+  imports = [ ./maintenance.nix ];
   enterTest = "";
 }
